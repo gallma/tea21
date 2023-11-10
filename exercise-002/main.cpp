414c8997ee0f2b5a2af22587101b7cde60da9e16
gallma <149794321+gallma@users.noreply.github.com>
2023-11-10 10:53:10 +0100

    Zufall

--- exercise-002/main.cpp
+++ exercise-002/main.cpp
@@ -10,26 +10,36 @@ auto main(int argc, char **argv) -> int
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
      */
-    
+
     int count =20;
+    std::srand(std::time(0));
     CLI::App app{PROJECT_NAME};
     try
     {
         app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
-        app.add_option("-c,--count", count, "A help string");
+        app.add_option("-c,--count", count, "Variable");
         app.parse(argc, argv);
     }
     catch (const CLI::ParseError &e)
     {
         return app.exit(e);
     }
+    std::vector<int> data(count);
+    std::srand(std::time(0));
+    std::generate(data.begin(), data.end(), []() { return std::rand() % 100 + 1; });
 
-    /**
+       /**
      * The {fmt} lib is a cross platform library for printing and formatting text
      * it is much more convenient than std::cout and printf
      * More info at https://fmt.dev/latest/api.html
      */
-    fmt::print("Hello, {}!\n", app.get_name());
+
+    fmt::print("Generated vector: ");
+    for (const auto &value : data)
+    {
+        fmt::print("{} ", value);
+    }
+    fmt::print("\n");
 
     /* INSERT YOUR CODE HERE */
 
