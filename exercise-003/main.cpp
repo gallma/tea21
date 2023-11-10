2e3db421859632c0d7c74865f13a8ee9a33a3102
gallma <149794321+gallma@users.noreply.github.com>
2023-11-10 14:24:25 +0100

    Zwischen

--- exercise-003/main.cpp
+++ exercise-003/main.cpp
@@ -10,38 +10,50 @@ void printer(const std::string &name)
 
 auto main(int argc, char **argv) -> int
 {
-    /**
-     * The {fmt} lib is a cross platform library for printing and formatting text
-     * it is much more convenient than std::cout and printf
-     * More info at https://fmt.dev/latest/api.html
-     */
     fmt::print("Hello, {}!\n", "World");
 
     const std::vector<std::string> names{"Hugo", "Franz", "Elisabeth", "Anton", "Gerhard", "Maria", "Hannelore"};
     const std::vector<std::string> names2{"Ulf", "Ole", "Sepp", "Norton", "Kai", "Uschi", "Gert"};
+
     {
         LinkedList list;
+
         fmt::print("------------------------------\n");
+
+        // Fügen Sie die Elemente aus 'names' am Ende der Liste hinzu
         for (auto &name : names)
         {
             auto elem = new LinkedListNode(name);
-            // elem->print();
             list.insert_tail(elem);
         }
+        
         list.traverse(printer);
+
         fmt::print("------------------------------\n");
+
+        // Fügen Sie die Elemente aus 'names2' am Anfang der Liste hinzu
         for (auto &name : names2)
         {
             auto elem = new LinkedListNode(name);
-            // elem->print();
             list.insert_head(elem);
         }
+
         fmt::print("------------------------------\n");
+
+        // Drucken Sie die Liste erneut aus
         list.traverse(printer);
+
         fmt::print("------------------------------\n");
+
+        // Anzahl der Elemente in der Liste ausgeben
         fmt::print("Elements in the list: {}\n", list.size());
+
         fmt::print("------------------------------\n");
+
+        // Die Liste wird erneut gedruckt, um zu überprüfen, ob die Elemente korrekt eingefügt wurden
         list.traverse(printer);
-    }         /* this calls the DTOR of the list*/
-    return 0; /* exit gracefully*/
+    }
+
+    return 0;
 }
+
